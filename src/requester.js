a4892b810106d285c879b0ba8446f8b977ed076e
Harry Stevens <harry.stevens@nytimes.com>
2025-11-13 13:44:29 -0500

    Add User-Agent to requester. Fixes #1.

--- src/requester.js
+++ src/requester.js
@@ -1,5 +1,24 @@
+const agents = [
+  // Chrome on Windows
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+  
+  // Chrome on macOS
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+  
+  // Firefox on Windows
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+  
+  // Safari on macOS
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
+];
+
 module.exports = function(parser, url, callback){
-  require("request")(url, function(err, res, body){
+  require("request")({
+    url,
+    headers: {
+      "User-Agent": agents[Math.floor(Math.random() * agents.length)]
+    }
+  }, function(err, res, body){
     if (err){
       console.log("Error requesting " + url, err);
       return;
